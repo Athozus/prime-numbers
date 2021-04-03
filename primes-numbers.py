7857154f266214bd5bf9bcff193ba7d52a3a2af3
AirSThib <75171564+AirSThib@users.noreply.github.com>
2021-04-03 11:56:55 +0200

    Update primes-numbers.py

--- primes-numbers.py
+++ primes-numbers.py
@@ -1,26 +1,56 @@
-from math import ceil
+from math import ceil, sqrt
 from time import time
 
 def primesNb(max, show=True):
-    if show:
-        time1 = time()
-        n = 0
+    time1 = time()
+    n = 0
+    try:
+        if max < 1024:
+            print(1/0)
         nbs = [True] * max + [True]
-        for i in range(2, max):
-            if nbs[i]:
-                print(i)
-                n +=1
-                for j in range(i*2, max, i):
-                    nbs[j] = False
-    else:
-        time1 = time()
-        n = 0
-        nbs = [True] * max + [True]
-        for i in range(2, max):
-            if nbs[i]:
-                n +=1
-                for j in range(i*2, max, i):
-                    nbs[j] = False
+        if show:
+             for i in range(2, max):
+                if nbs[i]:
+                    print(i)
+                    n +=1
+                    for j in range(i*2, max, i):
+                        nbs[j] = False
+        else:
+            for i in range(2, max):
+                if nbs[i]:
+                    n +=1
+                    for j in range(i*2, max, i):
+                        nbs[j] = False
+    except:
+        if show:
+            for i in range(1, max):
+                prime = True
+                if i == 2:
+                    pass
+                elif i%2 == 0:
+                    prime = False
+                else:
+                    for j in range(1, ceil(sqrt(max)), 2):
+                        if i%j == 0:
+                            prime = False
+                            break
+                if prime:
+                    n += 1
+                    print(i)
+        else:
+            for i in range(1, max):
+                prime = True
+                if i == 2:
+                    pas
+                elif i%2 == 0:
+                    prime = False
+                else:
+                    for j in range(1, ceil(sqrt(max)), 2):
+                        if i%j == 0:
+                            prime = False
+                            break
+                if prime:
+                    n += 1
     print("Examinated: " + str(max))
     print("Primes: " + str(n))
     print("Percentage : " + str(ceil(n/max*1000)/1000) + " %")
